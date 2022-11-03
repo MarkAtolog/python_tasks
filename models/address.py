37ade82a89e6e27a2bd7a3e56f734e4d817d4d4c
a.pushkov <mentalistone@gmail.com>
2022-11-03 15:43:19 +0400

    refactor json_repr for work with getters and setters

--- models/address.py
+++ models/address.py
@@ -14,7 +14,10 @@ class Address:
         self.index = index
 
     def json_repr(self):
-        return self.__dict__
+        new_dict = dict()
+        for key in self.__dict__:
+            new_dict[key.strip("_")] = self.__dict__[key]
+        return new_dict
 
     def __str__(self):
         return self.__dict__.__str__()
