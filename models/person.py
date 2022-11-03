735c874570ea9edd796afd557821eddef9832271
a.pushkov <mentalistone@gmail.com>
2022-11-03 15:40:07 +0400

    add some getters and setters

--- models/person.py
+++ models/person.py
@@ -1,5 +1,3 @@
-import json
-
 from models.address import Address
 from models.base_model import BaseModel
 
@@ -11,14 +9,44 @@ class Person(BaseModel):
                  age: int,
                  phone_number: list[str],
                  address: list[dict | Address]):
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.phone_number = phone_number
-        self.address = [Address(**item) for item in address]
+        self._name = name
+        self._surname = surname
+        self._age = age
+        self._phone_number = phone_number
+        self._address = [Address(**item) for item in address]
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str):
+            self._name = name
+
+    @property
+    def surname(self):
+        return self._surname
+
+    @surname.setter
+    def surname(self, surname):
+        if isinstance(surname, str):
+            self._surname = surname
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if isinstance(age, int):
+            self._age = age
 
     def json_repr(self):
-        return self.__dict__
+        new_dict = dict()
+        for key in self.__dict__:
+            new_dict[key.strip("_")] = self.__dict__[key]
+        return new_dict
 
     def __str__(self):
         return self.__dict__.__str__()
